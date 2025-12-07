b9c3a167ff4e910a96652ac359e887cba64cf4c9
Gopinah <gopinath24205@gmail.com>
2025-12-07 22:26:42 +0530

    redundancy controlled

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -2,10 +2,9 @@
 import mongoose from "mongoose";
 
 const contactSchema = new mongoose.Schema(
-  {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  {    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     name: { type: String },
-    phoneEncrypted: { type: String, required: true }, // AES encrypted phone
+    phoneEncrypted: { type: String, required: true, unique:true }, // AES encrypted phone
   },
   { timestamps: true }
 );
