a4a35e65f30ab6586f53ebb6a8054e9a4bd0a5e6
Shairaaj <iamshairaaj@gmail.com>
2026-07-14 18:18:54 +0530

    Ready for deployment steps. Completed migration from local Mongo compass to Atlas Cluster

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI || "mongodb://localhost:27017/swicall";
+    const uri = process.env.MONGO_URI;
     await mongoose.connect(uri);
     console.log("MongoDB connected to", uri);
   } catch (error) {
